2da6a8882d9c7d9cb9026001770af523dd63dfdb
Neha Sharma <nehavatsyan49207@gmail.com>
2019-02-26 06:15:31 +0530

    data types

---
+++ vardatatypes.java
@@ -0,0 +1,14 @@
+class vardatatypes{
+    public static void main(String args[]){
+        Boolean g = true;
+        byte a = 2;
+        int b = 100;
+        long c = 200000;
+        short d = 762;
+        float f = 3.2f;
+        double e  = 2;
+        char m = 'a';
+        String i = "hi";
+        System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+g+" "+i+" "+m);
+    }
+}
